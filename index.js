5c94104082ed90204e2034b6918b03a36a680bb5
Omar Hamdan <93857681+OmarHamdann@users.noreply.github.com>
2022-06-27 13:47:27 +0000

    done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+var os=require("os")
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -25,6 +26,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function (req, res) {
+  return res.json(
+    {
+      "ipaddress": req.ip,
+      "language": req.headers["accept-language"],
+      "software": req.headers["user-agent"]
+    }
+  );
+});
 
 
 // listen for requests :)
